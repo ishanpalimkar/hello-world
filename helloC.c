2f8fd25463ba41787d3a9e3dddff208237b86de0
Ishan <ishan0313@gmail.com>
2022-02-02 10:41:29 +0530

    Update helloC.c
    Added the functionality to get the input msg from user and print it

--- helloC.c
+++ helloC.c
@@ -2,6 +2,10 @@
 
 int main()
 {
-    printf("Hello World");
+    // add functionality to get a msg from user and print it
+    char str[MAX_LIMIT];
+    printf("Enter the msg you want to print");
+    fgets(str, MAX_LIMIT, stdin);
+    printf("%s", str);
     return 0;
 }
